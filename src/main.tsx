056a7e7fd6241be40b675292d7ab37683ee153d0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-12 17:04:06 -0300

    :sparkles: feat: Adiciona página de Inicio para ser renderizada

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
+import Inicio from '@/pages/Inicio'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        
+        <Inicio />
     </React.StrictMode>
 )
