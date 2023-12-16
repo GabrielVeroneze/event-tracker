50a3f1aea6bfd235fd4c2ec78b77070d8e45902c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-16 18:44:14 -0300

    :sparkles: feat: Cria funcionalidade de filtrar eventos

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -10,12 +10,22 @@ const ListaDeEventos: React.FC = () => {
     const { eventos } = useListaDeEventos()
     const filtro = useRecoilValue<IFiltroDeEventos>(filtroDeEventos)
 
+    const eventosFiltrados = eventos.filter(evento => {
+        if (!filtro.data) {
+            return true
+        }
+
+        const eventoFormatado = evento.inicio.toISOString().slice(0, 10)
+        const filtroFormatado = filtro.data.toISOString().slice(0, 10)
+
+        return eventoFormatado === filtroFormatado
+    })
 
     return (
         <section>
             <Filtro />
             <div className={styles.scroll}>
-                {eventos.map(evento => (
+                {eventosFiltrados.map(evento => (
                     <Evento evento={evento} key={evento.id} />
                 ))}
             </div>
