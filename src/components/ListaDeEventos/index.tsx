02736cd2d8ef4ada800dfd2bfc655d1b680d720f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-16 18:43:24 -0300

    :sparkles: feat: Utiliza useRecoilValue para acessar o valor do filtro

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -1,10 +1,15 @@
+import { useRecoilValue } from 'recoil'
+import { filtroDeEventos } from '@/state/atom'
 import { useListaDeEventos } from '@/hooks/useListaDeEventos'
+import { IFiltroDeEventos } from '@/interfaces/IFiltroDeEventos'
 import Filtro from '@/components/Filtro'
 import Evento from '@/components/Evento'
 import styles from './ListaDeEventos.module.scss'
 
 const ListaDeEventos: React.FC = () => {
     const { eventos } = useListaDeEventos()
+    const filtro = useRecoilValue<IFiltroDeEventos>(filtroDeEventos)
+
 
     return (
         <section>
