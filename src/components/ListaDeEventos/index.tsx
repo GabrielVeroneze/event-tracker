513327a1b62622fc2871c9c4d1d483baa8ed9e61
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-16 23:09:24 -0300

    :recycle: refactor: Extraí o filtrar de eventos para o selector

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -1,31 +1,16 @@
-import { useRecoilValue } from 'recoil'
-import { filtroDeEventos } from '@/state/atom'
 import { useListaDeEventos } from '@/hooks/useListaDeEventos'
-import { IFiltroDeEventos } from '@/interfaces/IFiltroDeEventos'
 import Filtro from '@/components/Filtro'
 import Evento from '@/components/Evento'
 import styles from './ListaDeEventos.module.scss'
 
 const ListaDeEventos: React.FC = () => {
     const { eventos } = useListaDeEventos()
-    const filtro = useRecoilValue<IFiltroDeEventos>(filtroDeEventos)
-
-    const eventosFiltrados = eventos.filter(evento => {
-        if (!filtro.data) {
-            return true
-        }
-
-        const eventoFormatado = evento.inicio.toISOString().slice(0, 10)
-        const filtroFormatado = filtro.data.toISOString().slice(0, 10)
-
-        return eventoFormatado === filtroFormatado
-    })
 
     return (
         <section>
             <Filtro />
             <div className={styles.scroll}>
-                {eventosFiltrados.map(evento => (
+                {eventos.map(evento => (
                     <Evento evento={evento} key={evento.id} />
                 ))}
             </div>
--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -7,5 +7,17 @@ export const eventosFiltradosState = selector({
         const filtro = get(filtroDeEventos)
         const todosOsEventos = get(listaDeEventosState)
 
+        const eventos = todosOsEventos.filter(evento => {
+            if (!filtro.data) {
+                return true
+            }
+
+            const eventoFormatado = evento.inicio.toISOString().slice(0, 10)
+            const filtroFormatado = filtro.data.toISOString().slice(0, 10)
+
+            return eventoFormatado === filtroFormatado
+        })
+
+        return eventos
     }
 })
