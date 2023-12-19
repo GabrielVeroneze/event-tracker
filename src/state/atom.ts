eddb7ccf61f80fa76e13eb1d3fe86e8111a52517
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-18 23:42:51 -0300

    :sparkles: feat: Define o valor inicial como o resultado do seletor eventosAsync

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,10 +1,11 @@
 import { atom } from 'recoil'
+import { eventosAsync } from '@/state/seletores'
 import { IEvento } from '@/interfaces/IEvento'
 import { IFiltroDeEventos } from '@/interfaces/IFiltroDeEventos'
 
 export const listaDeEventosState = atom<IEvento[]>({
     key: 'listaDeEventosState',
-    default: [],
+    default: eventosAsync,
 })
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
